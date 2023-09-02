1df5c92acdce1e5c9b545465c7eefbb87d3315a6
Jaime COSTEIRA <jaime.costeira@viacesi.fr>
2023-09-02 11:26:22 +0200

    Reduced number of particles for phones.

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,16 @@ window.onload = function() {
   Particles.init({
     selector: '.background',
     color: '#008fd1',
-    connectParticles: true
+    connectParticles: true,
+    responsive: [
+    {
+      breakpoint: 768,
+      options: {
+        maxParticles: 40,
+      }
+    }
+  ]
+
   });
 };
 </script>
